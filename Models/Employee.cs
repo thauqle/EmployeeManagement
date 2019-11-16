1d22e7bd0db159e731cfa6285e668728bbff62dd
thauqle <thauqle@yahoo.co.uk>
2019-11-16 21:52:50 +0000

    Tutorial up t 44

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,18 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Name cannot exceed 20 characters")]
         public string Name { get; set; }
-        public string Department { get; set; }
+
+        [Required]
+        public Dept? Department { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "Invalid Email Format")]
+        [Display(Name = "Office Email")]
         public string  Email { get; set; }
     }
 }
--- Models/MockEmployeeRepository.cs
+++ Models/MockEmployeeRepository.cs
@@ -13,11 +13,19 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@pragimtech.com" },
-                new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@pragimtech.com" },
-                new Employee() { Id = 3, Name = "Sam", Department = "RD", Email = "sam@pragimtech.com" },
+                new Employee() { Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@pragimtech.com" },
+                new Employee() { Id = 2, Name = "John", Department = Dept.IT, Email = "john@pragimtech.com" },
+                new Employee() { Id = 3, Name = "Sam", Department = Dept.Payroll, Email = "sam@pragimtech.com" }
             };
         }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e=>e.Id)+1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+
         public IEnumerable<Employee> GetAllEmployee()
         {
             return _employeeList;
