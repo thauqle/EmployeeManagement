b9b9b13d07de3a1b4e4c6b8009e45d5aea222885
Thau Le <Thau.Le@enghouse.com>
2019-11-22 17:20:14 +0000

    ASP.NET CORE TUTORIAL 55

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -44,6 +44,21 @@ namespace EmployeeManagement.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+            return View(employeeEditViewModel);
+        }
+
         [HttpPost]
         public IActionResult Create(EmployeeCreateViewModel model)
         {
